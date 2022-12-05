554bdf4b2bb5b3d97dcafa1e88d4fadef18cd50e
AsadbekAnvarov2305 <asadbekanvarov2305@gmail.com>
2022-12-05 17:41:19 +0500

    add login

--- js/index.js
+++ js/index.js
@@ -17,95 +17,95 @@ for (let i = 0; i < lists.length; i++) {
 
 
 
-// carusel start
+// // carusel start
 
 
-let left = document.querySelector(".left");
-let right = document.querySelector(".right");
-let hero = document.querySelector(".hero");
-let span1 = document.querySelector("#span1");
-let span2 = document.querySelector("#span2");
-let span3 = document.querySelector("#span3");
-let span4 = document.querySelector("#span4");
+// let left = document.querySelector(".left");
+// let right = document.querySelector(".right");
+// let hero = document.querySelector(".hero");
+// let span1 = document.querySelector("#span1");
+// let span2 = document.querySelector("#span2");
+// let span3 = document.querySelector("#span3");
+// let span4 = document.querySelector("#span4");
 
-let plus = 0
+// let plus = 0
 
-let active = document.querySelectorAll(".box1-span");
-
-
+// let active = document.querySelectorAll(".box1-span");
 
 
-right.addEventListener('click', () => {
-    console.log(right);
 
-    if(plus == -4050){
-        right.type.disabled
-    }
-    else{
-        hero.style.transform = `translateX(${plus -= 1350}px)`
-    }
+
+// right.addEventListener('click', () => {
+//     console.log(right);
+
+//     if(plus == -4050){
+//         right.type.disabled
+//     }
+//     else{
+//         hero.style.transform = `translateX(${plus -= 1350}px)`
+//     }
 
 
     
-} )
+// } )
 
-left.addEventListener('click', () => {
-    console.log(left);
+// left.addEventListener('click', () => {
+//     console.log(left);
 
-    if(plus == 0){
-        left.type.disabled
-    }
-    else{
+//     if(plus == 0){
+//         left.type.disabled
+//     }
+//     else{
 
-        hero.style.transform = `translateX(${plus += 1350}px)`
-    }
+//         hero.style.transform = `translateX(${plus += 1350}px)`
+//     }
     
-} )
+// } )
 
 
-let dot = 0
+// let dot = 0
 
-span1.addEventListener("click", () => {
-if(plus == -1350){
-    span3.type.disabled
-}
-else{
-    hero.style.transform= `translateX(${dot+0}px)`
+// span1.addEventListener("click", () => {
+// if(plus == -1350){
+//     span3.type.disabled
+// }
+// else{
+//     hero.style.transform= `translateX(${dot+0}px)`
 
-}
-});
+// }
+// });
 
-span2.addEventListener("click", () => {
-if(dot == -800){
-span2.type.disabled
-}
-else{
-    hero.style.transform= `translateX(${dot-1350}px)`
+// span2.addEventListener("click", () => {
+// if(dot == -800){
+// span2.type.disabled
+// }
+// else{
+//     hero.style.transform= `translateX(${dot-1350}px)`
 
-}
-});
+// }
+// });
 
-span3.addEventListener("click", () => {
-if(plus == -800){
-    span3.type.disabled
-}
-else{
-    hero.style.transform= `translateX(${dot-2700}px)`
+// span3.addEventListener("click", () => {
+// if(plus == -800){
+//     span3.type.disabled
+// }
+// else{
+//     hero.style.transform= `translateX(${dot-2700}px)`
 
-}
-});
-span4.addEventListener("click", () => {
-if(plus == -800){
-    span4.type.disabled
-}
-else{
-    hero.style.transform= `translateX(${dot-4050}px)`
+// }
+// });
+// span4.addEventListener("click", () => {
+// if(plus == -800){
+//     span4.type.disabled
+// }
+// else{
+//     hero.style.transform= `translateX(${dot-4050}px)`
 
-}
-});
+// }
+// });
 
 
-// carusel end
+// // carusel end
 
 
 
