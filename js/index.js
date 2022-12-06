bd2e71dd60d45c3235782c134966601d96c73199
AsadbekAnvarov2305 <asadbekanvarov2305@gmail.com>
2022-12-06 17:51:34 +0500

    add login

--- js/index.js
+++ js/index.js
@@ -17,96 +17,6 @@ for (let i = 0; i < lists.length; i++) {
 
 
 
-// // carusel start
-
-
-// let left = document.querySelector(".left");
-// let right = document.querySelector(".right");
-// let hero = document.querySelector(".hero");
-// let span1 = document.querySelector("#span1");
-// let span2 = document.querySelector("#span2");
-// let span3 = document.querySelector("#span3");
-// let span4 = document.querySelector("#span4");
-
-// let plus = 0
-
-// let active = document.querySelectorAll(".box1-span");
-
-
-
-
-// right.addEventListener('click', () => {
-//     console.log(right);
-
-//     if(plus == -4050){
-//         right.type.disabled
-//     }
-//     else{
-//         hero.style.transform = `translateX(${plus -= 1350}px)`
-//     }
-
-
-    
-// } )
-
-// left.addEventListener('click', () => {
-//     console.log(left);
-
-//     if(plus == 0){
-//         left.type.disabled
-//     }
-//     else{
-
-//         hero.style.transform = `translateX(${plus += 1350}px)`
-//     }
-    
-// } )
-
-
-// let dot = 0
-
-// span1.addEventListener("click", () => {
-// if(plus == -1350){
-//     span3.type.disabled
-// }
-// else{
-//     hero.style.transform= `translateX(${dot+0}px)`
-
-// }
-// });
-
-// span2.addEventListener("click", () => {
-// if(dot == -800){
-// span2.type.disabled
-// }
-// else{
-//     hero.style.transform= `translateX(${dot-1350}px)`
-
-// }
-// });
-
-// span3.addEventListener("click", () => {
-// if(plus == -800){
-//     span3.type.disabled
-// }
-// else{
-//     hero.style.transform= `translateX(${dot-2700}px)`
-
-// }
-// });
-// span4.addEventListener("click", () => {
-// if(plus == -800){
-//     span4.type.disabled
-// }
-// else{
-//     hero.style.transform= `translateX(${dot-4050}px)`
-
-// }
-// });
-
-
-// // carusel end
-
 
 
 // sms start
@@ -126,29 +36,7 @@ del.addEventListener('click', () => {
 })
 
 
-// let chat = document.querySelector(".last");
 
-// function ekrangaChiqarish(){
-
-//     let res = " "
-//     `
-//     <div class="last__img">
-//         <a href="#">
-//             <img src="./img/sms__user.svg" alt="user-sms" width="28" height="29">
-//         </a>
-//     </div>
-//     <div class="last__chat">
-//         <p>
-//             Salom 😊 
-//             Keling, tadbiringiz uchun sayt topishga yordam beraylik.
-//             Sana haqida qaror qabul qildingizmi?
-//         </p>
-//     </div>
-//     `
-    
-// }
-
-// ekrangaChiqarish()
 
 
 
@@ -187,3 +75,16 @@ for (let i = 0; i < services.length; i++) {
 	});
 }
 // services end
+
+
+// login
+
+let loginTopLeft = document.querySelectorAll(".login__left");
+let loginTopRight = document.querySelectorAll(".login__right");
+
+
+let loginBody = document.querySelector(".login__left-body");
+
+loginTopLeft.addEventListener("click", () => {
+	
+})
---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+
+let loginTopLeft = document.querySelectorAll(".login__left");
+let loginTopRight = document.querySelectorAll(".login__right");
+
+
+let loginBody = document.querySelector(".login__left-body");
+
+loginTopLeft.addEventListener("click" , () => {
+    console.log('asadbek');
+})
